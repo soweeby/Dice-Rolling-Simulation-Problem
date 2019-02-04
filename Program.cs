dee7438572f9f5730e06e6c4bae439d381a19b70
roshan117 <rgnunna@gmail.com>
2019-02-03 19:10:34 -0500

    first version

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace dice_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input dice configuration string");
+            //reads user input for the dice notation (adn, "a" being # of sides of dice, "n" being the # of sides on the dice)
+            string die = Console.ReadLine();
+            //splits up the dice notation into readable parts (EX: 3d12 reads as rolling a 12 sided dice 3 times)
+            string[] components = die.Split("d");
+            //checks first part of dice notation for any letters
+            bool part1wrong = components[0].Any(x => char.IsLetter(x));
+            //checks second part of dice notation for any letters
+            bool part2wrong = components[1].Any(x => char.IsLetter(x));
+            
+            //if either part of the parsed dice notation contains any letters, then it is invalid and the user is prompted to try again
+            if (part1wrong == true || part2wrong == true) {
+                Console.WriteLine("Bad String, try again");
+            }
+            else {
+            //initializes a new dice object according to the # of rolls to be done and # of sides it has
+            Dice die_components = new Dice(Convert.ToInt32(components[0]), Convert.ToInt32(components[1]));
+            //shows the results of each roll and the total number of all the roll values combined
+            die_components.DoSimulation();
+            
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ dice.cs
@@ -0,0 +1,31 @@
+using System;
+namespace dice_problem {
+    public class Dice {
+        private int sides;
+        private int rolls;
+
+
+        public Dice(int rolls, int sides) {
+            this.sides = sides;
+            this.rolls = rolls;
+        }
+        //does a new roll and returns the number given from the roll
+        public int newroll() {
+            Random rand = new Random();
+            int rolled = rand.Next(sides) + 1;
+            return rolled;
+        }
+        //I really need to clean up the way I went about showing the simulation and total, it is very messy (but it does the job for now)
+        public int DoSimulation() {
+            int total = 0;
+            for (int x = 1; x <= rolls; x++) {
+                int rollednumber = newroll();
+                Console.WriteLine("Roll {0}: {1}", x, rollednumber);
+                total = total + rollednumber;
+
+            }
+            Console.WriteLine("Total: {0}", total);
+            return total;
+        }
+    }
+}
